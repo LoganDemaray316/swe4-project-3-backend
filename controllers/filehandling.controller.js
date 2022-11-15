a4aa2cb41dffa07f79144aa5d3003211783bc2ee
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-11-14 21:21:13 -0600

    Add parse file function

---
+++ controllers/filehandling.controller.js
@@ -0,0 +1,14 @@
+function parseFile(file) {
+  let buildings = [];
+  let course = [];
+  let events = [];
+  let faculties = [];
+  let facultysections = [];
+  let favorites = [];
+  let rooms = [];
+  let sections = [];
+  let sectiontimes = [];
+  let semesters = [];
+  let sessions = [];
+  let users = [];
+}
