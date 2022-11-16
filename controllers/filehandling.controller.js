c5fd147c22c13516b694ada9f41d493ec72991ec
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-11-15 23:55:08 -0600

    Update to create data on database

--- controllers/filehandling.controller.js
+++ controllers/filehandling.controller.js
@@ -1,12 +1,15 @@
+const { sequelize } = require("../models");
+const db = require("../models");
+
+const Buildings = db.buildings;
+const Faculty = db.faculty;
+const Rooms = db.rooms;
+const Sections = db.sections;
+const SectionTime = db.sectiontime;
+const Semesters = db.semesters;
+
 function parseFile(file) {
   const data = $.csv.toObjects(file);
-  let buildings = [{}];
-  let faculties = [{}];
-  let facultysections = [{}];
-  let rooms = [{}];
-  let sections = [{}];
-  let sectiontimes = [{}];
-  let semesters = [{}];
 
   for (let i = 0; i < data.length; i++) {
     delete data[i]["Synonym"];
@@ -53,45 +56,78 @@ function parseFile(file) {
 
   for (let i = 0; i < data.length; i++) {
     let temp = {};
-    if (!buildings.some((e) => e.name === data[i]["Bldg"])) {
-      temp.name = data[i]["Bldg"];
-    }
+
+    temp.name = data[i]["Bldg"];
+
     if (!temp === {}) {
-      buildings.push(temp);
+      Buildings.create(temp);
       temp = {};
     }
-    if (
-      !faculties.some(
-        (e) => e.name === data[i]["Faculty First"] + data[i]["Faculty Last"]
-      )
-    ) {
-      temp.name = data[i]["Faculty First"] + data[i]["Faculty Last"];
-    }
+    temp.name = data[i]["Faculty First"] + " " + data[i]["Faculty Last"];
     if (!temp === {}) {
-      faculties.push(temp);
+      Faculty.create(temp);
       temp = {};
     }
-    if (!semesters.some((e) => e.code === data[i]["Term"])) {
-      temp.code = data[i]["Term"];
-    }
-    if (semesters.some((e) => e.startDate === data[i]["Start Date"])) {
-      //Do nothing, that value is already there
-    } else if (semesters.forEach((e) => e.startDate > data[i]["Start Date"])) {
-      let x = semesters.findIndex((e) => e.startDate > data[i]["Start Date"]);
-      semesters[x].startDate = data[i]["Start Date"];
-    } else {
-      temp.startDate = data[i]["Start Date"];
-    }
-    if (semesters.some((e) => e.endDate === data[i]["End Date"])) {
-      //Do nothing, that value is already there
-    } else if (semesters.forEach((e) => e.endDate < data[i]["End Date"])) {
-      let x = semesters.findIndex((e) => e.endDate < data[i]["End Date"]);
-      semesters[x].endDate = data[i]["End Date"];
-    } else {
-      temp.endDate = data[i]["End Date"];
-    }
+    temp.code = data[i]["Term"];
+    temp.startDate = data[i]["Start Date"];
+    temp.endDate = data[i]["End Date"];
     if (!temp === {}) {
-      semesters.push(temp);
+      Semesters.create(temp);
+      temp = {};
+    }
+    temp.room = data[i]["Rooms"];
+    temp.buildingId = sequelize.query(
+      `select id from buildings where name like ${data[i]["Bldg"]}`
+    );
+    if (!temp === {}) {
+      Rooms.create(temp);
+      temp = {};
+    }
+    temp = {};
+    temp.number =
+      data[i]["Subject"] +
+      "-" +
+      data[i]["Course #"] +
+      "-" +
+      data[i]["Section #"];
+    temp.courseId = sequelize.query(
+      `select id from courses where number like ${
+        data[i]["Subject"] + "-" + data[i]["Course #"]
+      }`
+    );
+    temp.capacity = parseInt(data[i]["Capacity"]);
+    temp.semesterId = sequelize.query(
+      `select id from semesters where code like ${data[i]["Term"]}`
+    );
+    if (!temp === {}) {
+      Sections.create(temp);
+      temp = {};
+    }
+    temp = {};
+    temp.sectionId = sequelize.query(
+      `select id from sections where number like ${
+        data[i]["Subject"] +
+        "-" +
+        data[i]["Course #"] +
+        "-" +
+        data[i]["Section #"]
+      }`
+    );
+    temp.startTime = data[i]["Start Time"];
+    temp.endTime = data[i]["End Time"];
+    temp.startDate = data[i]["Start Date"];
+    temp.endDate = data[i]["End Date"];
+    temp.daysOfWeek = data[i]["Days"];
+    temp.roomId = sequelize.query(
+      `select rooms.id from rooms join buildings on buildings.id = rooms.buildingId where rooms.number like ${data[i]["Room"]} and buildings.name like ${data[i]["Bldg"]}`
+    );
+    temp.facultyId = sequelize.query(
+      `select id from faculties where name like ${
+        data[i]["Faculty First"] + " " + data[i]["Faculty Last"]
+      }`
+    );
+    if (!temp === {}) {
+      SectionTime.create(temp);
       temp = {};
     }
   }
